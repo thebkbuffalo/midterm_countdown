6b1abd6df948ec9e0f4d113719450bf4ffc9b733
Evan Berg <evan.m.berg@gmail.com>
2017-03-16 23:12:32 -0700

    location working way faster now.

--- main.js
+++ main.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
     clockFace: 'DailyCounter',
     countdown: true
   });
+
+  $('.house_txt').empty();
+  $('.senate_txt').empty();
+  $.getJSON("http://freegeoip.net/json/", function(data) {
+    var state = data.region_name
+    var time_zone = data.time_zone;
+    var latitude = data.latitude;
+    var longitude = data.longitude;
+    var house_url = 'https://en.wikipedia.org/wiki/United_States_House_of_Representatives_elections,_2018#/' + state
+    var senate_url = 'https://en.wikipedia.org/wiki/United_States_Senate_elections,_2018#/' + state
+    var house_link = $('.house_btn');
+    var senate_link = $('.senate_btn');
+
+    house_link.attr('href', house_url);
+    senate_link.attr('href', senate_url);
+    $('.house_txt').append('(' + state + ')');
+    $('.senate_txt').append('(' + state + ')');
+
+  });
 });
