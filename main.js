53b200fa624d868466b185695c10c0f93d510d32
Evan Berg <evan.m.berg@gmail.com>
2017-05-26 23:43:29 -0700

    ugh

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
     $('.senate_txt').append('(' + state + ')');
 
     var zip = data.postal
-    $.getJSON('https://congress.api.sunlightfoundation.com/legislators/locate?zip=' + zip, function(rep_data){
+    $.getJSON('http://congress.api.sunlightfoundation.com/legislators/locate?zip=' + zip, function(rep_data){
       rep_data = rep_data.results
       $.each(rep_data, function(k, v){
         name = v.first_name +  ' ' + v.last_name;
@@ -55,4 +55,27 @@ $(document).ready(function(){
 
 
   });
+  function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
 });
